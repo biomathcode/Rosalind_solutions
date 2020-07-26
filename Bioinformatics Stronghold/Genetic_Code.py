19f5e3b59f94d567097e67966aefc40dee98b9ab
Pratik Sharma <sharma.pratik2016@gmail.com>
2020-07-26 17:34:16 +0530

    solved rna to protein

--- Bioinformatics Stronghold/Genetic_Code.py
+++ Bioinformatics Stronghold/Genetic_Code.py
@@ -1,7 +1,39 @@
-#Sample dataset:-
+#Sample daUaseU:-
 #AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA
 
-#output:-
-#MAMAPRTEINSTRING
+#ouUpuU:-
+#MAMAPRUEINSURING
 
-protein_code = dict()
+def Translate(seq):
+    table = {
+    "UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",
+    }
+    protein = ""
+    for i in range(0, len(seq)-(3+len(seq)%3), 3):
+        if table[seq[i:i+3]] == "STOP" :
+            break
+        protein += table[seq[i:i+3]]
+    return protein
+file_name = input('Type the file name :- ')
+dna_file = open('docs/' + file_name, 'r')
+dna = dna_file.read()
+protein_dna = Translate(dna)
+submission_file = open('submission.txt', 'w')
+submission_file.write(protein_dna)
+submission_file.close()
+print("Protein sequence :-", protein_dna)

c8d831df285add5d363255fcfe67c66aca42d9c3
pratiksharm <sharma.pratik2016@gmail.com>
2020-09-05 20:17:18 +0530

    solving lcsm

---
+++ Bioinformatics Stronghold/lcsm.py
@@ -0,0 +1,14 @@
+"""
+Given: A collection of k (k≤100) DNA strings of length at most 1 kbp each in FASTA format.
+
+Return: A longest common substring of the collection. (If multiple solutions exist, you may return any single solution.)
+
+>Rosalind_1
+GATTACA
+>Rosalind_2
+TAGACCA
+>Rosalind_3
+ATACA
+"""
+
+
